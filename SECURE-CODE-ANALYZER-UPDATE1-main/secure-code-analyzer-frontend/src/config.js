ad348635d2d98f69cde8900368bbea3488003887
Pravinkumarg18 <pravcs104@rmkcet.ac.in>
2025-09-02 22:39:06 +0530

    update config.js

--- SECURE-CODE-ANALYZER-UPDATE1-main/secure-code-analyzer-frontend/src/config.js
+++ SECURE-CODE-ANALYZER-UPDATE1-main/secure-code-analyzer-frontend/src/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "https://secure-code-analyzer-update1.onrender.com:10000";
+const API_BASE_URL = "https://secure-code-analyzer-update1.onrender.com";
+
 
 export default API_BASE_URL;
